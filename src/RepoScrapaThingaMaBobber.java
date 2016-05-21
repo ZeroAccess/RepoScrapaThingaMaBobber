597a5c097014837e60a57b75d0cfaf5747d9daba
Glen Roberts <glensroberts@gmail.com>
2016-05-21 05:27:22 -0400

    Added Lambda expression to iterate over directories. Directory Structure cannot revert back to previous good copy. Working on that and then once the directory structure is complete will add the files in and work on formatting.

--- src/RepoScrapaThingaMaBobber.java
+++ src/RepoScrapaThingaMaBobber.java
@@ -1,36 +1,49 @@
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 
 /**
  * Created by robertsg on 5/20/2016.
  **/
 
-public class RepoScrapaThingaMaBobber {
+class RepoScrapaThingaMaBobber {
+    private String url;
+    private final StringBuilder stringBuilder = new StringBuilder();
 
     public static void main(String[] args) {
         RepoScrapaThingaMaBobber program = new RepoScrapaThingaMaBobber();
-        program.retrieveUrl();
+        program.BuildUri("");
     }
 
-    private void retrieveUrl() {
-        //Todo Use this method to create a menu to retrieve multiple versions
-        String url = "http://archive.ubuntu.com/ubuntu/pool/";
-
-        try {
-            Document doc = Jsoup.connect(url).userAgent("Chrome").get();
-            System.out.println(doc.title() +"\n");
-
-
-            Elements repoPackages = doc.select("dt");
-            for (Element repoPackage : repoPackages) {
-                System.out.println("Package : " + repoPackage.select("a[href]").text() + repoPackage.select("dt").text());
-            }
-        } catch (IOException ex) {
+    private void BuildUri(String buildUrl) {
+         try {
+             url = new URI("http://archive.ubuntu.com/ubuntu/pool/"
+                    + stringBuilder.append(buildUrl).toString()).toString();
+        } catch (URISyntaxException ex) {
             ex.printStackTrace();
+        }
+        displayDirectories();
+    }
+
+    private void displayDirectories() {
+           try {
+            Document doc = Jsoup.connect(url).userAgent("Chrome").get();
+            Elements repoPackages = doc.select("tr");
+            repoPackages.stream()
+                    .filter(repoPackage -> !repoPackage.select("a[href]").text().isEmpty() && repoPackage.select("a[href]").text().endsWith("/"))
+                    .map(repoPackage -> { System.out.println(url); return repoPackage;})
+                    .forEach(repoPackage -> BuildUri(repoPackage.select("a[href]").text()));
+        } catch (NullPointerException e) {
+            System.out.println("NullPointer Exception");
+        } catch (HttpStatusException e) {
+            System.out.println("HttpStatus Exception");
+        } catch (IOException e) {
+            System.out.println("IOException Exception");
         }
     }
 }
